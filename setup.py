47ff908d083c5cc70aaaffe4a9db8fe00d95f581
Ian Flanagan <150696975+flanagi1@users.noreply.github.com>
2026-04-08 09:32:52 -0400

    setup.py
    Just for downloading so it happens once

---
+++ setup.py
@@ -0,0 +1,6 @@
+import nltk
+nltk.download('punkt')
+nltk.download('punkt_tab') # Added this for newer NLTK versions
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('averaged_perceptron_tagger_eng') # This is the specific fix for your error 
